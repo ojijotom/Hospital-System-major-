8779535ce8ae8b5d4d7875bc00f53e83c3e81aba
Mugashtess <nliz72361@gmail.com>
2025-03-12 16:13:09 +0300

    ojijo

--- Hospitalsystem/settings.py
+++ Hospitalsystem/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'Hospitalsystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':  'hospitalsystem',
+        'USER':  'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -108,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
